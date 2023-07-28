f46cce65ecdf061623625ebe2bc25e8295eab2e1
cat <colacat1927@gmail.com>
2023-07-28 15:43:40 +0300

    добавил бургер

--- src/js/app.js
+++ src/js/app.js
@@ -2,4 +2,18 @@ import * as flsFunctions from "./modules/functions.js";
 
 document.addEventListener("DOMContentLoaded", () => {
 	flsFunctions.isWebp();
+
+
+	const burger = document.querySelector(".burger");
+
+	burger.addEventListener("click", toggleBurger);
+
+	function toggleBurger() {
+		const overlay = document.querySelector(".overlay");
+		if (JSON.parse(burger.getAttribute("aria-expanded"))) {
+			overlay.classList.add("active");
+		} else {
+			overlay.classList.remove("active");
+		}
+	}
 });

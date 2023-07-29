c4b931bc3046ac308019ce3eb5dc482efb44b125
cat <colacat1927@gmail.com>
2023-07-29 12:09:09 +0300

    добавил адаптив для хедера

--- src/js/app.js
+++ src/js/app.js
@@ -10,10 +10,16 @@ document.addEventListener("DOMContentLoaded", () => {
 
 	function toggleBurger() {
 		const overlay = document.querySelector(".overlay");
+		const header = document.querySelector(".header");
+		const intro = document.querySelector(".intro");
 		if (JSON.parse(burger.getAttribute("aria-expanded"))) {
 			overlay.classList.add("active");
+			header.classList.add("active");
+			intro.classList.add("active");
 		} else {
 			overlay.classList.remove("active");
+			header.classList.remove("active");
+			intro.classList.remove("active");
 		}
 	}
 });

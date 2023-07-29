cc79d02901c99aefa0f26317bb98208b0b58cf5e
cat <colacat1927@gmail.com>
2023-07-29 12:38:00 +0300

    мелкие правки в блоке intro, header

--- src/js/app.js
+++ src/js/app.js
@@ -10,16 +10,16 @@ document.addEventListener("DOMContentLoaded", () => {
 
 	function toggleBurger() {
 		const overlay = document.querySelector(".overlay");
-		const header = document.querySelector(".header");
 		const intro = document.querySelector(".intro");
+		const burger = document.querySelector(".burger");
 		if (JSON.parse(burger.getAttribute("aria-expanded"))) {
 			overlay.classList.add("active");
-			header.classList.add("active");
 			intro.classList.add("active");
+			burger.classList.add("active");
 		} else {
 			overlay.classList.remove("active");
-			header.classList.remove("active");
 			intro.classList.remove("active");
+			burger.classList.remove("active");
 		}
 	}
 });
